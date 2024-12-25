8194df78eb5a3d5fbcfc83a433a85683910864a4
Nasif <nasifabdullah396@gmail.com>
2024-12-26 03:30:54 +0800

    adding two url in app file

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import ProfilePage from "./pages/ProfilePage";
 // import useLocalStorage from "use-local-storage";
 import { EditContext } from "./EditContext";
 // import { PostArrayContext } from "./PostsArrayContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
-  const apiURL = 'https://vercel-express-blog-api.vercel.app/';
-  // const blog_API_URL = 'https://vercel-express-blog-api.vercel.app/';
+  const apiURL = 'https://vercel-express-blog-api.vercel.app';
+  const blog_API_URL = 'https://vercel-express-blog-api.vercel.app/';
 
-  // const [page, setPage] = useState();
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the Express API
+    axios
+      .get(blog_API_URL)
+      .then((response) => setPage(response.data))
+      .catch((error) => console.error("Error fetching the message:", error));
+
+  }, []);
 
   // const [posts, setPosts] = useState([]);     // 1 user, Many posts   [{post.id, post.created_at, post.updated_at, post.title, post.content}]
 
@@ -24,15 +34,20 @@ export default function App() {
   console.log('editId', editId);
 
   return (
-    // <PostArrayContext.Provider value={{posts, setPosts}}>
-      <EditContext.Provider value={{isEdit, setIsEdit, editId, setEditId}}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<AuthPage apiURL={apiURL} />} />
-            <Route path="/profile" element={<ProfilePage apiURL={apiURL} />} />
-          </Routes>
-        </BrowserRouter>
-      </EditContext.Provider>
-    // </PostArrayContext.Provider>
+    <>
+    {/* // <PostArrayContext.Provider value={{posts, setPosts}}> */}
+      {page ? ( 
+        <EditContext.Provider value={{isEdit, setIsEdit, editId, setEditId}}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<AuthPage apiURL={apiURL} />} />
+              <Route path="/profile" element={<ProfilePage apiURL={apiURL} />} />
+            </Routes>
+          </BrowserRouter>
+        </EditContext.Provider>
+      )
+      : 'Loading data...'}
+    {/* // </PostArrayContext.Provider> */}
+    </>
   );
 }
